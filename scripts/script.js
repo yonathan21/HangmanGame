a28fb3220f584219c373b74937ebc9fd3428d5cc
yonathan21 <eldanadereje06@gmail.com>
2025-02-21 21:51:49 +0300

    displayingwordinplaceofdash

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,34 @@
 const keyboardDiv = document.querySelector(".keyboard");
 const wordDisplay = document.querySelector(".word-display");
 
+let currentWord;
+
 const getRandomWord = () => {
   const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
   console.log(word);
-
+  currentWord = word;
   document.querySelector(".hint-text").innerText = hint;
   wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
+}
+
+const initGame = (button, clickedLetter) => {
+  if(currentWord.includes(clickedLetter)){
+    [...currentWord].forEach((letter, index) => {
+      if(letter === clickedLetter){
+        wordDisplay.querySelectorAll("li")[index].innerText = letter;
+        wordDisplay.querySelectorAll("li")[index].classList.add("guessed");
+      }
+    })
+  }else{
+    console.log(clickedLetter, " is not exist on the letter"); 
+  }
 }
 
 for (let i = 97; i <= 122; i++){
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
+  button.addEventListener("click", e => initGame(e.target,  String.fromCharCode(i)));
 }
 
 getRandomWord();
