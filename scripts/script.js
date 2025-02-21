e35afe43061693c34a518ac4ffbacbaa495a75af
yonathan21 <eldanadereje06@gmail.com>
2025-02-21 21:01:53 +0300

    creatingwordlist

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
 const keyboardDiv = document.querySelector(".keyboard");
 
+const getRandomWord = () => {
+  const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
+  console.log(word,hint);
+}
+
 for (let i = 97; i <= 122; i++){
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
 }
+
+getRandomWord();
