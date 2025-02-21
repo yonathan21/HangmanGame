c9ec2f9f75533aa5fddeb0e5b3f0036cbac9845e
yonathan21 <eldanadereje06@gmail.com>
2025-02-21 21:36:55 +0300

    displayingword

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,12 @@
 const keyboardDiv = document.querySelector(".keyboard");
+const wordDisplay = document.querySelector(".word-display");
 
 const getRandomWord = () => {
   const {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
   console.log(word);
 
   document.querySelector(".hint-text").innerText = hint;
+  wordDisplay.innerHTML = word.split("").map(() => `<li class="letter"></li>`).join("");
 }
 
 for (let i = 97; i <= 122; i++){
